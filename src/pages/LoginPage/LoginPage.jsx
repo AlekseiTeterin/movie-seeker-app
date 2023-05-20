b8523f60adb2c13e662c7a524eb5fa588c464ab4
Aleksei Teterin <alexey_teterin@mail.ru>
2023-05-21 00:06:32 +0300

    Добавлен базовый роутинг, базовая версия header с переходами на страницы-заглушки

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './LoginPage.module.css';
+
+function LoginPage() {
+    return (
+        <div className={style.login}>
+            Здесь позже будет страница авторизации
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './RegisterPage.module.css';
+
+function RegisterPage() {
+    return (
+        <div className={style.register}>
+            Здесь позже будет страница регистрации
+        </div>
+    );
+}
+
+export default RegisterPage;
