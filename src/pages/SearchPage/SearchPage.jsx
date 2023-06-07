a9699ceaa039c1cea19b2a79a3614d4185089e21
AlekseiTeterin <alexey_teterin@mail.ru>
2023-06-07 18:39:44 +0300

    добавлен на странице результатов поиска текст запроса

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -8,13 +8,18 @@ function SearchPage() {
     const filmName = useParams().query;
     const { data, isLoading, error } = useGetMovieByNameQuery(filmName);
     return (
-        <div className={style.searchPage}>
-            {isLoading && <div className={style.movie}>Loading...</div>}
-            {error && <div className={style.movie}>Something error. Oops.</div>}
-            {data &&
-                data.map((film) => (
-                    <MovieCard key={film.id} movieId={film.id} />
-                ))}
+        <div>
+            <div className={style.name}>{filmName}</div>
+            <div className={style.searchPage}>
+                {isLoading && <div className={style.movie}>Loading...</div>}
+                {error && (
+                    <div className={style.movie}>Something error. Oops.</div>
+                )}
+                {data &&
+                    data.map((film) => (
+                        <MovieCard key={film.id} movieId={film.id} />
+                    ))}
+            </div>
         </div>
     );
 }
